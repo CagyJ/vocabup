cf2f4e7cd0710f87f2ebfa1b710a8bd4cec595f0
CagyJ <josh.jiao.322@gmail.com>
2022-08-04 10:34:21 +0200

    :sparkles: add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Layout from "./components/Layout";
-import Home from "./components/pages//Home";
+// import Home from "./components/pages//Home";
+import Singup from "./components/pages/Signup";
+// import Login from "./components/pages/Login";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Home />
+        {/* <Home /> */}
+        <Singup />
+        {/* <Login /> */}
       </Layout>
     </div>
   );
