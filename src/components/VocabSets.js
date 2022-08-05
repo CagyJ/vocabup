2b3889d9fb716aa2d7f7a8a9d3f949bc90953c56
CagyJ <josh.jiao.322@gmail.com>
2022-08-05 09:02:10 +0200

    :bug: chang all links to use Link instead

--- src/components/VocabSets.js
+++ src/components/VocabSets.js
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
 import classes from "../styles/VocabSets.module.css";
 import VocabSet from "./VocabSet";
 
 const VocabSets = () => {
   return (
     <div className={classes.sets}>
-      <VocabSet />
-      <VocabSet />
-      <VocabSet />
-      <VocabSet />
-      <VocabSet />
-      <VocabSet />
-      <VocabSet />
+      <Link to="/quiz">
+        <VocabSet />
+      </Link>
+      <Link to="/quiz">
+        <VocabSet />
+      </Link>
+      <Link to="/quiz">
+        <VocabSet />
+      </Link>
+      <Link to="/quiz">
+        <VocabSet />
+      </Link>
+      <Link to="/quiz">
+        <VocabSet />
+      </Link>
     </div>
   );
 };
