d311ee7340b632049c173093b4ee6f089da51cad
CagyJ <josh.jiao.322@gmail.com>
2022-08-06 10:03:03 +0200

    :art: refactor signup page including a SignupForm component

---
+++ src/components/SignupForm.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import Form from "./Form";
+import TextInput from "./TextInput";
+const SignupForm = () => {
+  return (
+    <Form style={{ height: "500px" }}>
+      <TextInput type="text" placeholder="Enter name" icon="person" />
+
+      <TextInput type="text" placeholder="Enter email" icon="alternate_email" />
+
+      <TextInput type="password" placeholder="Enter password" icon="lock" />
+
+      <TextInput
+        type="password"
+        placeholder="Confirm password"
+        icon="lock_clock"
+      />
+
+      <TextInput type="text" placeholder="Access Code" icon="key" />
+
+      <Checkbox text="I agree to the Terms &amp; Conditions" />
+
+      <Button>
+        <span>Signup Now</span>
+      </Button>
+
+      <div className="info">
+        Already have an account? <Link to="/login">Login</Link> instead.
+      </div>
+    </Form>
+  );
+};
+
+export default SignupForm;
