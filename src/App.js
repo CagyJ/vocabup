2be8880846125d0f58d4133b8584ecb7e0bceac7
CagyJ <josh.jiao.322@gmail.com>
2022-08-07 10:20:30 +0200

    :lock: extend custom Route for security

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from "./components/pages/Login";
 import Quiz from "./components/pages/Quiz";
 import Result from "./components/pages/Result";
 import Signup from "./components/pages/Signup";
+import PrivateRoute from "./components/route/PrivateRoute";
+import PublicRoute from "./components/route/PublicRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./styles/App.css";
 
@@ -15,10 +17,42 @@ function App() {
         <Layout>
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/quiz" element={<Quiz />} />
-            <Route exact path="/result" element={<Result />} />
+            <Route
+              exact
+              path="/signup"
+              element={
+                <PublicRoute>
+                  <Signup />
+                </PublicRoute>
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              exact
+              path="/quiz"
+              element={
+                <PrivateRoute>
+                  <Quiz />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              exact
+              path="/result"
+              element={
+                <PrivateRoute>
+                  <Result />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </Layout>
       </AuthProvider>
