bdb6d4fb69420710ff367153b4f2cebb7ddc4e1a
CagyJ <josh.jiao.322@gmail.com>
2022-08-14 16:00:29 +0200

    :sparkles: introduce infinite scroll with pagination

--- src/components/VocabSets.js
+++ src/components/VocabSets.js
@@ -1,25 +1,34 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
 import useVocabSets from "../hooks/useVocabSets";
-import classes from "../styles/VocabSets.module.css";
 import VocabSet from "./VocabSet";
 
 const VocabSets = () => {
-  const { loading, error, vocabSets } = useVocabSets();
+  const [page, setPage] = useState(() => 1);
+  const { loading, error, vocabSets, hasMore } = useVocabSets(page);
 
   console.log(vocabSets);
   return (
-    <div className={classes.sets}>
-      {vocabSets.length > 0 &&
-        vocabSets.map((set) => (
-          <Link to="/quiz" key={set.id}>
-            <VocabSet
-              title={set.title}
-              id={set.id}
-              num={set.num}
-              times={set.times}
-            />
-          </Link>
-        ))}
+    <div>
+      {vocabSets.length > 0 && (
+        <InfiniteScroll
+          dataLength={vocabSets.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 12)}
+        >
+          {vocabSets.map((set) => (
+            <Link to="/quiz" key={set.id}>
+              <VocabSet
+                title={set.title}
+                id={set.id}
+                num={set.num}
+                times={set.times}
+              />
+            </Link>
+          ))}
+        </InfiniteScroll>
+      )}
       {loading && <div>Loading...</div>}
       {error && <div>Error</div>}
       {!loading && vocabSets.length === 0 && <div>No data exists.</div>}
--- src/hooks/useVocabSets.js
+++ src/hooks/useVocabSets.js
@@ -1,23 +1,39 @@
-import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
 import { useEffect, useState } from "react";
 
-const useVocabSets = () => {
+const useVocabSets = (page) => {
   console.log("useVocabSets starts");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [vocabSets, setVocabSets] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     console.log("useEffect starts");
-    fetchSets();
-  }, []);
+    fetchSets(page);
+  }, [page]);
 
-  const fetchSets = async () => {
+  const fetchSets = async (page) => {
     console.log("fetchSets starts");
     const db = getDatabase();
     const setsRef = ref(db, "sets");
 
-    const setsQuery = query(setsRef, orderByKey());
+    console.log("Page: " + page);
+
+    const setsQuery = query(
+      setsRef,
+      orderByKey(),
+      startAt(String(page)),
+      limitToFirst(12)
+    );
 
     try {
       setError(false);
@@ -26,12 +42,19 @@ const useVocabSets = () => {
       const snapshot = await get(setsQuery);
       console.log(snapshot);
       setLoading(false);
+
       if (snapshot.exists()) {
         console.log(snapshot.val());
         setVocabSets((prevSets) => {
-          return [...Object.values(snapshot.val())];
+          if (prevSets.length === 0) {
+            return [...Object.values(snapshot.val())];
+          }
+          return isEqual(prevSets, [...Object.values(snapshot.val())])
+            ? [...prevSets]
+            : [...prevSets, ...Object.values(snapshot.val())];
         });
       } else {
+        setHasMore(false);
       }
     } catch (err) {
       console.log(err);
@@ -43,7 +66,22 @@ const useVocabSets = () => {
     loading,
     error,
     vocabSets,
+    hasMore,
   };
+};
+
+const isEqual = (one, another) => {
+  const len1 = one.length;
+  const len2 = another.length;
+  for (var i = 0; i < len1; i++) {
+    const o1 = one[i];
+    const o2 = another[i];
+    const isSame = o1.id === o2.id && o1.title === o2.title;
+    if (!isSame) {
+      return false;
+    }
+  }
+  return true && len1 === len2;
 };
 
 export default useVocabSets;
